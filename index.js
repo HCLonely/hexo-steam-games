5329d58b1702cb4db11a10f7f06626a1ac1c0cab
HCLonely <h1606051253@gmail.com>
2020-02-09 21:40:29 +0800

    first publish

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const log = require('hexo-log')({
     debug: false,
     silent: false
 });
-const axios = require('axios');
+const axios = require('axios-https-proxy-fix');
 const cheerio = require('cheerio');
+
 hexo.extend.generator.register('steamgames', function (locals) {
     if (!this.config.steam || !this.config.steam.enable) {
         return;
@@ -20,45 +21,53 @@ hexo.extend.console.register('steam update', 'Generate pages from steamgames', f
         log.info("Please add steamId to _config.yml");
         return;
     }
-    updateSteamGames(this.config.steam.steamId, this.config.steam.tab ||"recent",this.config.steam.length);
+    updateSteamGames(this.config.steam.steamId, this.config.steam.tab,this.config.steam.length,this.config.steam.proxy);
 });
 
-function updateSteamGames(steamId, tab,length){
+function updateSteamGames(steamId, tab = "recent", length=1000, proxy=false) {
     log.info("Getting steam games, please wait...");
-    let options={
+    let options = {
         method: "GET",
         url: `https://steamcommunity.com/profiles/${steamId}/games/?tab=${tab}`,
         timeout: 0,
         responseType: "text",
-        headers:{
+        headers: {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
         }
     };
-
-    axios(options).then(response=>{
-        if(response.status===200){
+    if (proxy) {
+        options.proxy = {
+            host: proxy.host,
+            port: proxy.port
+        };
+    }
+    axios(options).then(response => {
+        if (response.status === 200) {
             let $ = cheerio.load(response.data);
             let script = $('script[language="javascript"]');
-            var games=[];
-            for (let i = 0; i < script.length;i++){
-                if (script.eq(i).html().includes("rgGames")){
+            var games = [];
+            for (let i = 0; i < script.length; i++) {
+                if (script.eq(i).html().includes("rgGames")) {
                     let rgGames = script.eq(i).html().match(/var.*?rgGames.*?=.*?(\[[\w\W]*?\}\}\]);/);
-                    if (rgGames){
+                    if (rgGames) {
                         games = JSON.parse(rgGames[1]);
                         break;
                     }
                 }
             }
-            fs.writeFile("./data/games.json", JSON.stringify(games.slice(0,length||1000)),err=>{
-                if(err){
+            if (!fs.existsSync("./data/")) {
+                fs.mkdirsSync("./data/");
+            }
+            fs.writeFile("./data/games.json", JSON.stringify(games.slice(0, length)), err => {
+                if (err) {
                     log.info("Failed to write data to games.json");
                     console.log(err);
-                }else{
+                } else {
                     log.info("Get steam games: complete!");
                 }
             });
         }
-    }).catch(error=>{
+    }).catch(error => {
         console.log(error);
     });
 }
